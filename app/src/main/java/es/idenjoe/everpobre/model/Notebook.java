ccd0f63c8722a049ed9d163f1a62015727f87c8f
José Manuel Rodríguez Moreno <idenjoe@idenjoe.es>
2016-05-07 18:02:25 +0200

    Added constructors

--- app/src/main/java/es/idenjoe/everpobre/model/Notebook.java
+++ app/src/main/java/es/idenjoe/everpobre/model/Notebook.java
@@ -7,6 +7,16 @@ public class Notebook {
     private int id;
     private String name;
 
+
+    public Notebook(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    private Notebook(){
+        //disable default constructor, customer requirement
+    }
+
     public int getId() {
         return id;
     }
